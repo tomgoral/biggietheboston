df26abed9bf2e66127d806e088f712a34abb357d
Tom Goral <tgoral@acument.com>
2021-06-21 10:36:53 -0400

    adding random dog pic

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+Challenge:
+
+1. Fetch a random image from the Dog API again 
+(https://dog.ceo/api/breeds/image/random)
+
+2. Access the DOM and insert the URL you got from the
+API as an image `src` property (probably easiest if 
+you create the image completely here in the JS and add 
+it as the innerHTML of another element on the DOM)
+*/
+
+fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById("image-container").innerHTML = `
+            <img src="${data.message}" />
+        `
+    })
